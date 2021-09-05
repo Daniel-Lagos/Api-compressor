016058ad80fc1297304229c5d531c97f75f0aca9
Daniel Lagos <daniel.lagos010@gmail.com>
2021-09-04 21:29:42 -0500

    rename zip file and remove file when its is upload

--- controller/upload.js
+++ controller/upload.js
@@ -5,26 +5,29 @@ const JSZip = require('jszip');
 const uploadFile = async (req, res = response) => {
   const zip = new JSZip();
   const files = req.files || [];
-  const images = zip.folder('images');
+  const zipName = req.body.fileName || new Date().getTime();
 
   files.map((file) => {
     fs.renameSync(file.path, file.originalname);
     const path = file.originalname;
-    images.file(path,
+    zip.file(path,
       fs.readFileSync(path),
       { base64: true });
   });
 
   const content = await zip.generateAsync({ type: 'nodebuffer' });
-  // const nameZip = 'fecha del documento';
-  fs.writeFileSync('files.zip', content);
+  fs.writeFileSync(zipName + '.zip', content);
 
   files.map((file) => {
     fs.unlinkSync(file.originalname);
   });
 
-  res.download('./files.zip');
-  // fs.unlinkSync('Borrar el zip');
+  res.download('./' + zipName + '.zip', function (err) {
+    if (err) {
+      console.log(err);
+    }
+    fs.unlinkSync('./' + zipName + '.zip');
+  });
 };
 
 module.exports = {
