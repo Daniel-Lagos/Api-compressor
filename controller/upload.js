9be492ba88ab08bd41513ac8ae4a48d0f0175039
Daniel Lagos <d.f.l.m.0126@gmail.com>
2021-10-08 01:01:01 -0500

    changes

--- controller/upload.js
+++ controller/upload.js
@@ -6,7 +6,7 @@ const path = require('path');
 const uploadFile = async (req, res = response) => {
   const zip = new JSZip();
   const files = req.files || [];
-  const zipName = req.body.fileName || new Date().getTime();
+  const zipName = req.body.fileName || `${new Date().getTime()}`;
 
   files.map((file) => {
     fs.renameSync(file.path, file.originalname);
@@ -16,21 +16,32 @@ const uploadFile = async (req, res = response) => {
       { base64: true });
   });
 
-  const content = await zip.generateAsync({ type: 'nodebuffer' });
-  fs.writeFileSync(zipName + '.zip', content);
+  const content = await zip.generateAsync({ type: 'base64' });
+
 
   files.map((file) => {
     fs.unlinkSync(file.originalname);
   });
 
-  res.sendFile(path.resolve(`./${zipName}.zip`), function (err) {
+  return res.send(content);
+
+};
+
+const downloadFile = (req, res = response) => {
+
+  const zipName = req.params.id;
+
+  return res.download(path.resolve(`./${zipName}.zip`), function (err) {
     if (err) {
       console.log(err);
     }
-    fs.unlinkSync('./' + zipName + '.zip');
+    
   });
+
 };
 
+
 module.exports = {
-  uploadFile
+  uploadFile,
+  downloadFile
 };
--- router/upload.js
+++ router/upload.js
@@ -1,10 +1,12 @@
 const { Router } = require('express');
-const { uploadFile } = require('../controller/upload');
+const { uploadFile, downloadFile } = require('../controller/upload');
 const multer = require('multer');
 const upload = multer({ dest: './' });
 
 const router = Router();
 
 router.post('/upload', upload.array('field', 5), uploadFile);
+
+router.get('/download/:id', [], downloadFile);
 
 module.exports = router;
